ccfe7a3f67f64468c95aa1f4549c84cd8bacd2b9
YRTech <yrtechx@gmail.com>
2023-10-21 20:49:42 +0200

    done project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angulartask';
+  title = 'angulartask'
+  password: string = ''
+  error = ''
+
+  getPasswordStrength(): string {
+    const lengthCondition = this.password.length >= 8
+    const hasLetters = /[a-zA-Z]/.test(this.password)
+    const hasDigits = /\d/.test(this.password)
+    const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(this.password)
+    if(!lengthCondition && this.password.trim().length){
+      this.error = 'The password is too short'
+      return 'short'
+    } 
+     if (hasLetters && hasDigits && hasSymbols) {
+      this.error = 'The password is strong'
+      return 'strong'
+    } else if ((hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols)) {
+      this.error = 'The password is medium'
+      return 'medium'
+    } else if (hasLetters || hasDigits || hasSymbols) {
+      this.error = 'The password is easy'
+      return 'easy'
+    }
+    this.error = 'The password is empty'
+    return ''
+  }
 }
