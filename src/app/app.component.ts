9a2e60e6902df4ff36bfd67f9f44149525350add
YRTech <yrtechx@gmail.com>
2023-10-25 10:44:30 +0200

    with ControlValueAccessor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+// app.component.ts
+import { Component, forwardRef } from '@angular/core';
+import { FormGroup, FormBuilder, NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import { PasswordService } from 'src/services/passwordStrengthService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => AppComponent),
+      multi: true,
+    },
+    PasswordService,
+  ],
 })
-export class AppComponent {
-  title = 'angulartask';
+export class AppComponent implements ControlValueAccessor {
   passwordForm: FormGroup;
-  error: string = '';
+  error: string = ''
 
   constructor(private fb: FormBuilder, private passwordService: PasswordService) {
     this.passwordForm = this.fb.group({
@@ -21,6 +29,22 @@ export class AppComponent {
   getPasswordStrength(): string {
     const password = this.passwordForm.get('password')?.value;
     this.error = this.passwordService.getPasswordStrength(password);
-    return this.error;
+    return this.error
+  }
+
+  writeValue(obj: any): void {
+    this.passwordForm.setValue({ password: obj }, { emitEvent: false });
+  }
+
+  registerOnChange(fn: any): void {
+    this.passwordForm.valueChanges.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    this.passwordForm.valueChanges.subscribe(fn);
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.passwordForm.disable() : this.passwordForm.enable();
   }
 }
--- src/services/passwordStrengthService.ts
+++ src/services/passwordStrengthService.ts
@@ -1,28 +1,44 @@
 import { Injectable } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PasswordService {
+export class PasswordService implements ControlValueAccessor {
 
-  getPasswordStrength(password: string): string {
-    const lengthCondition = password.length >= 8;
-    const hasLetters = /[a-zA-Z]/.test(password);
-    const hasDigits = /\d/.test(password);
-    const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-
-    if (!lengthCondition && password.trim().length) {
-      return 'short';
+    private onChange: (value: any) => void = () => {}
+    private onTouch: () => void = () => {}
+  
+    writeValue(obj: any): void {}
+  
+    registerOnChange(fn: any): void {
+      this.onChange = fn
     }
-
-    if (hasLetters && hasDigits && hasSymbols) {
-      return 'strong';
-    } else if ((hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols)) {
-      return 'medium';
-    } else if (hasLetters || hasDigits || hasSymbols) {
-      return 'easy';
+  
+    registerOnTouched(fn: any): void {
+      this.onTouch = fn
     }
+  
+    setDisabledState?(isDisabled: boolean): void {}
 
-    return '';
-  }
+    getPasswordStrength(password: string): string {
+        const lengthCondition = password.length >= 8
+        const hasLetters = /[a-zA-Z]/.test(password)
+        const hasDigits = /\d/.test(password)
+        const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+
+        if (!lengthCondition && password.trim().length) {
+        return 'short'
+        }
+
+        if (hasLetters && hasDigits && hasSymbols) {
+        return 'strong'
+        } else if ((hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols)) {
+        return 'medium'
+        } else if (hasLetters || hasDigits || hasSymbols) {
+        return 'easy'
+        }
+
+        return ''
+    }
 }
